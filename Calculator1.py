17f233704e70f5378d385583570558eff5a2c082
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-01 17:18:41 -0700

    added code
    calculator code

---
+++ Calculator1.py
@@ -0,0 +1,3 @@
+name = input("What is your name?")
+print("Hello , " + name + " let's find out how many full moons you have lived through...")
+birthmonth = int(input("What month of the year where you born? Enter only the month number"))
