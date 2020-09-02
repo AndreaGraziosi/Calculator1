e2e91370e13faabb67082c904a9655246e92e6ef
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-01 17:19:32 -0700

    added notes and more inputs

--- Calculator1.py
+++ Calculator1.py
@@ -1,3 +1,6 @@
 name = input("What is your name?")
 print("Hello , " + name + " let's find out how many full moons you have lived through...")
 birthmonth = int(input("What month of the year where you born? Enter only the month number"))
+currentmonth = int(input("What is the month of the year today? Enter only month number "))
+#print("wow! "  + name! Since your birth there have been  full Moons!)
+birthyear = int(input("What year were you born? Write down the four numbers of birthyear "))
