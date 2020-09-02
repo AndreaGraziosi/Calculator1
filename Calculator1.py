d65549231fd55258619a152561155c24e423d1e9
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-01 17:58:01 -0700

    updated descriptions

--- Calculator1.py
+++ Calculator1.py
@@ -8,4 +8,7 @@ number_of_moons = (2019-(birthyear+1))*12 + ((12 - birthmonth)+ (12 - currentmon
 print(str(number_of_moons)+ " Full Moons!")
 #This calculator is called Many Moons. The calculator returns an approximate cnumber of how many full moons (excluding blue moons, the 13th moon certain years experienve) that have risen in a persons life time!
 #I used Python 3.7.9 to create Many Moons
-
+# I used the input() so that I could allow for user input
+#I wanted to communicate a hello message to the reader so in line 2 I used a Hello message. The message will use the information stored in name, inputed by the user in line 1
+# I converted ny numerical inputs from string to int() so that I could use those in a numerical formula
+#I stored the result of my formula in the variable number_of_moons so that I could print a concatenated message for the user (converted the value in number_of_moons to a string so that I could add extra text for the message)
