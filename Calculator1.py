d5725923f6face53aa46cb48062708995c8b1c61
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-01 17:20:14 -0700

    Final print statement

--- Calculator1.py
+++ Calculator1.py
@@ -4,3 +4,5 @@ birthmonth = int(input("What month of the year where you born? Enter only the mo
 currentmonth = int(input("What is the month of the year today? Enter only month number "))
 #print("wow! "  + name! Since your birth there have been  full Moons!)
 birthyear = int(input("What year were you born? Write down the four numbers of birthyear "))
+number_of_moons = (2019-(birthyear+1))*12 + ((12 - birthmonth)+ (12 - currentmonth))
+print(str(number_of_moons)+ " Full Moons!")
